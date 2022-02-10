74a2732930a9156c171a9a6068fb231dc530376c
StevenT07 <57376186+StevenT07@users.noreply.github.com>
2022-02-10 14:05:57 -0800

    Added Expression Binary Tree

--- DerivativeCalculator.java
+++ DerivativeCalculator.java
@@ -1,39 +1,43 @@
+import java.util.Stack;
+
 public class DerivativeCalculator {
+    private final char[] operators = {'+', '*', '-', '/', '^'};
     public DerivativeCalculator() {
     }
     //  worry about input later
 
     // input Formatting: ex. (x + 1)
+    // this sets up the tree
     public String derive(String input) {
         input = input.replaceAll(" ", "");
-        // base cases
-        if ( input.matches("[0-9]+")) { // if the input is constant
-            return "0";
-        }
-        else if (input.contains("x")) {//
-            try  {
-                return "" + input.charAt(input.indexOf("x") - 1);
+        Stack<ExpressionNode> stack = new Stack<ExpressionNode>();
+        ExpressionNode temp;
+        for(int i = 0; i < input.length(); i++) {
+            if(!containsOperator(input.charAt(i))) {
+                temp = new ExpressionNode(input.charAt(i)+ "");
+                stack.push(temp);
             }
-            catch(StringIndexOutOfBoundsException e){
-                return "1";
+            else { // if is an operator
+                temp = new ExpressionNode(input.charAt(i) + "");
+                temp.right = stack.pop();
+                temp.left = stack.pop();
+                stack.push(temp);
             }
         }
-
-        else {
-            String result = "";
-            String[] terms = input.split("\\+");
-            for(int i = 0; i < terms.length; i++) {
-                result += derive(terms[i]);
-                if (i != terms.length - 1) {
-                    result += " + ";
-                }
-            }
-            return result;
-        }
+        temp = stack.pop();
 
 
+        return derive(temp);
     }
 
+    // this recurses through the tree
+    private String derive(ExpressionNode root) {
+        if (root != null) {
+            if (containsOperator(root.value.charAt(0))) {
+                
+            }
+        }
+    }
 
 
     private String powerRule(String input) {
@@ -48,7 +52,30 @@ public class DerivativeCalculator {
     private String quotientRule(String input) {
         return "";
     }
+    private boolean containsOperator(char str) {
+        for(char operator: operators) {
+            if(str == operator) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // binary expression tree
+    //
+    private class ExpressionNode {
+        public ExpressionNode left;
+        public ExpressionNode right;
+        public String value;
 
+        public ExpressionNode(String value)  {
+            this.value = value;
+        }
+        public ExpressionNode(String left, String right, String value) {
+            this.left = new ExpressionNode(left);
+            this.right = new ExpressionNode(right);
+            this.value = value;
 
+        }
+    }
 
 }
