b4f8df584ffb2fc9de0c92a077bd7356a7f49505
Hai Dang Nguyen <haidangn@uw.edu>
2022-04-07 22:18:25 -0700

    Started derive function

--- DerivativeCalculator.java
+++ DerivativeCalculator.java
@@ -1,50 +1,174 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+import java.lang.Character;
 
 public class DerivativeCalculator {
     private final char[] operators = {'+', '*', '-', '/', '^'};
-    public DerivativeCalculator() {
+
+    private char var;
+    public DerivativeCalculator(String var) {
+        this.var = var;
     }
     //  worry about input later
 
     // input Formatting: ex. (x + 1)
     // this sets up the tree
-    public String derive(String input) {
-        input = input.replaceAll(" ", "");
-        Stack<ExpressionNode> stack = new Stack<ExpressionNode>();
-        ExpressionNode temp;
-        for(int i = 0; i < input.length(); i++) {
-            if(!containsOperator(input.charAt(i))) {
-                temp = new ExpressionNode(input.charAt(i)+ "");
-                stack.push(temp);
+    //
+    public ExpressionNode makeTree(String s) {
+        s = s.replaceAll(" ", "");
+        Stack<ExpressionNode> stN = new Stack<ExpressionNode>();
+        Stack<String> stC = new Stack<>();
+
+        ExpressionNode t, t1, t2;
+
+        int[] p = new int[123];
+        p['+'] = p['-'] = 1;
+        p['/'] = p['*'] = 2;
+        p['^'] = 3;
+        p[')'] = 0;
+        Map<String, Integer> opPriority = new HashMap<>();
+
+        opPriority.put("+", 1);
+        opPriority.put("-", 1);
+        opPriority.put("/", 2);
+        opPriority.put("*", 2);
+        opPriority.put("^", 3);
+        String checkNum = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == '(') {
+
+                // Push '(' in char stack
+                stC.add("" + s.charAt(i));
             }
-            else { // if is an operator
-                temp = new ExpressionNode(input.charAt(i) + "");
-                temp.right = stack.pop();
-                temp.left = stack.pop();
-                stack.push(temp);
+
+            // Push the operands in node stack
+            else if (Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i)))
+            {
+                if (!Character.isDigit(s.charAt(i+1))) {
+                    checkNum += s.charAt(i) ;
+                    t = new ExpressionNode(checkNum);
+                    stN.add(t);
+                    checkNum = "";
+                }
+                else {
+                    checkNum +=  s.charAt(i);
+                }
+
+            }
+            else if (p[s.charAt(i)] > 0)
+            {
+
+                // If an operator with lower or
+                // same associativity appears
+                while (
+                        opPriority.containsKey(stC.peek()) && !stC.isEmpty() && !stC.peek().equals("(")
+                                && ((s.charAt(i) != '^' && opPriority.get(stC.peek()) >= p[s.charAt(i)])
+                                || (s.charAt(i) == '^'
+                                && opPriority.get(stC.peek()) > p[s.charAt(i)])))
+                {
+
+                    // Get and remove the top element
+                    // from the character stack
+                    t = new ExpressionNode(stC.peek());
+                    stC.pop();
+
+                    // Get and remove the top element
+                    // from the node stack
+                    t1 = stN.peek();
+                    stN.pop();
+
+                    // Get and remove the currently top
+                    // element from the node stack
+                    t2 = stN.peek();
+                    stN.pop();
+
+                    // Update the tree
+                    t.left = t2;
+                    t.right = t1;
+
+                    // Push the node to the node stack
+                    stN.add(t);
+                }
+
+                // Push s[i] to char stack
+                stC.push(s.charAt(i) + "");
+            }
+            else if (s.charAt(i) == ')') {
+                while (!stC.isEmpty() && !stC.peek().equals("("))
+                {
+                    t = new ExpressionNode(stC.peek());
+                    stC.pop();
+                    t1 = stN.peek();
+                    stN.pop();
+                    t2 = stN.peek();
+                    stN.pop();
+                    t.left = t2;
+                    t.right = t1;
+                    stN.add(t);
+                }
+                stC.pop();
             }
         }
-        temp = stack.pop();
-
-
-        return derive(temp);
+        t = stN.peek();
+        return t;
     }
 
     // this recurses through the tree
-    private String derive(ExpressionNode root) {
+    // should look through the code
+    /*
+    private ExpressionNode derive(ExpressionNode root) {
         if (root != null) {
             if (containsOperator(root.value.charAt(0))) {
-                
+
             }
         }
     }
 
+     */
+
 
     private String powerRule(String input) {
+
         return "";
 
     }
 
+    private String derive(ExpressionNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.value.equals(var)) {
+            return 1;
+        }
+        if (root.value.equals("+")){
+
+        }
+        if (root.value.equals("-")){
+
+        }
+        if (root.value.equals("/")){
+
+        }
+        if (root.value.equals("*")){
+
+        }
+        if (root.value.equals("+")){
+
+        }
+        if (root.value.equals("+")){
+
+        }
+        /* first print data of node */
+        System.out.print(root.key + " ");
+
+        /* then recur on left subtree */
+        derive(root.left);
+
+        /* now recur on right subtree */
+        derive(root.right);
+    }
     private String productRule(String input) {
         return "";
     }
